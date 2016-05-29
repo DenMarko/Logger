564f7933c37aaf3363b36498945eb6935234d7f9
DenMarko <denmarko@ukr.net>
2016-05-29 21:45:30 +0300

    Create Types.h

---
+++ Types.h
@@ -0,0 +1,18 @@
+#ifndef HEDER_FILE_TYPES
+#define HEDER_FILE_TYPES
+
+namespace _core
+{
+
+#define _DEBUG_BREAK_IF( _CONDITION_ )
+
+typedef		__int32		s32;
+
+typedef		unsigned short	u16;
+typedef		unsigned int	u32;
+
+typedef		char		c8;
+typedef		const char	cc8;
+
+};
+#endif // !HEDER_FILE_TYPES
